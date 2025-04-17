25373cafc8f69dc4042a32b60ba2072c35a55623
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-17 17:40:42 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    TOKEN_PARTS: {
+        PART1: "ghp_UCTgHQsAY5MjZ9AFye5",
+        PART2: "NPKty4Z4nt62H5cta"
+    },
+    REPO: {
+        OWNER: "mohammad-shehadeh",
+        NAME: "Iam"
+    },
+    FILE_PATH: "Server.md"
+};
+
+function assembleGitHubToken() {
+    return CONFIG.TOKEN_PARTS.PART1 + CONFIG.TOKEN_PARTS.PART2;
+}
