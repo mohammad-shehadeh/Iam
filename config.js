fac1912a4f7da2467a577af4a1d5e8e5bb30c318
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-17 19:51:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,11 @@
 const CONFIG = {
-    ENCODED_TOKEN: [
-        "noise1", "Z2hw",         // [1]
-        "X1VD", "junk",           // [2]
-        "VGdI", "UXNB",           // [4,5]
-        "WTVN", "random",         // [6]
-        "alpa", "OUFG",           // [8,9]
-        "eWU1", "TlBL",           // [10,11]
-        "dHk0", "WjRu",           // [12,13]
-        "dDYy", "SDVj", "dGE=",   // [14,15,16]
-        "end", "noise2"
-    ],
-    TOKEN_INDEXES: [
-        1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16
-    ],
-    REPO: {
-        OWNER: "mohammad-shehadeh",
-        NAME: "Iam"
+    TOKEN_PARTS: {
+        PART1: "ghp_UCTgHQsAY5MjZ9AFye5", // الجزء الأول من التوكن
+        PART2: "NPKty4Z4nt62H5cta"        // الجزء الثاني
     },
-    FILE_PATH: "Server.md"
+    REPO: {
+        OWNER: "mohammad-shehadeh",       // اسم المستخدم
+        NAME: "Iam"                       // اسم المستودع
+    },
+    FILE_PATH: "Server.md"               // اسم الملف داخل المستودع
 };
